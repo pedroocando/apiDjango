5c841c807039e3faec63908bfccea63615d39216
Pedro Ocando <pedro.ocando@binaural.com.ve>
2017-08-16 05:35:23 -0400

    s

--- api/settings.py
+++ api/settings.py
@@ -91,11 +91,26 @@ DATABASES = {
         'HOST':'ec2-54-235-120-39.compute-1.amazonaws.com',
         'PORT': 5432,
     }
+}
 
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 
 # Internationalization
